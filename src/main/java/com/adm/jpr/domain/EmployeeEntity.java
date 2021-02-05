49c6d2adaae5071155c1a1942bcf534b3f689a50
pjg <pedroraitz@gmail.com>
2021-02-05 18:12:14 -0300

    atualizando

--- src/main/java/com/adm/jpr/domain/EmployeeEntity.java
+++ src/main/java/com/adm/jpr/domain/EmployeeEntity.java
@@ -9,32 +9,110 @@ import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Entity
-@Table(name="employee")
+@Table(name = "employee")
 public class EmployeeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String firstName;
-	
+
 	private String lastName;
 
 	private String socialSecurityNumber;
 
-	private Double grossSales; 
+	private Double grossSales;
 
-	private Double commissionRate; 
+	private Double commissionRate;
 
-	private Double baseSalary; 
+	private Double baseSalary;
 
-	private Double wage; 
+	private Double wage;
 
-	private Double hours; 
+	private Double hours;
 
 	private Double weeklySalary;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getSocialSecurityNumber() {
+		return socialSecurityNumber;
+	}
+
+	public void setSocialSecurityNumber(String socialSecurityNumber) {
+		this.socialSecurityNumber = socialSecurityNumber;
+	}
+
+	public Double getGrossSales() {
+		return grossSales;
+	}
+
+	public void setGrossSales(Double grossSales) {
+		this.grossSales = grossSales;
+	}
+
+	public Double getCommissionRate() {
+		return commissionRate;
+	}
+
+	public void setCommissionRate(Double commissionRate) {
+		this.commissionRate = commissionRate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Double getWage() {
+		return wage;
+	}
+
+	public void setWage(Double wage) {
+		this.wage = wage;
+	}
+
+	public Double getHours() {
+		return hours;
+	}
+
+	public void setHours(Double hours) {
+		this.hours = hours;
+	}
+
+	public Double getWeeklySalary() {
+		return weeklySalary;
+	}
+
+	public void setWeeklySalary(Double weeklySalary) {
+		this.weeklySalary = weeklySalary;
+	}
 
 }
--- src/main/java/com/adm/jpr/service/EmployeeService.java
+++ src/main/java/com/adm/jpr/service/EmployeeService.java
@@ -3,6 +3,11 @@ package com.adm.jpr.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.adm.jpr.domain.EmployeeEntity;
+import com.adm.jpr.employee.BasePlusCommissionEmployee;
+import com.adm.jpr.employee.CommissionEmployee;
+import com.adm.jpr.employee.HourlyEmployee;
+import com.adm.jpr.employee.SalariedEmployee;
 import com.adm.jpr.repository.EmployeeRepository;
 
 @Component
@@ -10,4 +15,64 @@ public class EmployeeService {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
+
+	private EmployeeEntity convertSalariedEmployeeToEmployeeEntity(SalariedEmployee salariedEmployee) {
+		EmployeeEntity entity = new EmployeeEntity();
+		entity.setFirstName(salariedEmployee.getFirstName());
+		entity.setWeeklySalary(salariedEmployee.getWeeklySalary());
+		entity.setLastName(salariedEmployee.getLastName());
+		entity.setSocialSecurityNumber(salariedEmployee.getSocialSecurityNumber());
+		return entity;
+	}
+
+	public void saveSalariedEmployee(SalariedEmployee salariedEmployee) {
+		employeeRepository.saveAndFlush(convertSalariedEmployeeToEmployeeEntity(salariedEmployee));
+	}
+
+//	public void saveSalariedEmployee(SalariedEmployee salariedEmployee) {
+//	EmployeeEntity e = convertSalariedEmployeeToEmployeeEntity(salariedEmployee);
+//	employeeRepository.saveAndFlush(e);
+//}
+
+	private EmployeeEntity convertBasePlusCommissionEmployee(BasePlusCommissionEmployee basePlusCommissionEmployee) {
+		EmployeeEntity entity = new EmployeeEntity();
+		entity.setFirstName(basePlusCommissionEmployee.getFirstName());
+		entity.setLastName(basePlusCommissionEmployee.getLastName());
+		entity.setSocialSecurityNumber(basePlusCommissionEmployee.getSocialSecurityNumber());
+		entity.setBaseSalary(basePlusCommissionEmployee.getBaseSalary());
+		return entity;
+	}
+
+	public void saveBasePlusCommissionEmployee(BasePlusCommissionEmployee basePlusCommissionEmployee) {
+		employeeRepository.saveAndFlush(convertBasePlusCommissionEmployee(basePlusCommissionEmployee));
+	}
+
+	private EmployeeEntity convertCommissionEmployee(CommissionEmployee commissionEmployee) {
+		EmployeeEntity entity = new EmployeeEntity();
+		entity.setFirstName(commissionEmployee.getFirstName());
+		entity.setLastName(commissionEmployee.getLastName());
+		entity.setSocialSecurityNumber(commissionEmployee.getSocialSecurityNumber());
+		entity.setGrossSales(commissionEmployee.getGrossSales());
+		entity.setCommissionRate(commissionEmployee.getCommissionRate());
+		return entity;
+	}
+
+	public void saveCommissionEmployee(CommissionEmployee commissionEmployee) {
+		employeeRepository.saveAndFlush(convertCommissionEmployee(commissionEmployee));
+	}
+
+	private EmployeeEntity convertHourlyEmployee(HourlyEmployee hourlyEmployee) {
+		EmployeeEntity entity = new EmployeeEntity();
+		entity.setFirstName(hourlyEmployee.getFirstName());
+		entity.setLastName(hourlyEmployee.getLastName());
+		entity.setSocialSecurityNumber(hourlyEmployee.getSocialSecurityNumber());
+		entity.setWage(hourlyEmployee.getWage());
+		entity.setHours(hourlyEmployee.getHours());
+		return entity;
+	}
+
+	public void saveHourlyEmployee(HourlyEmployee hourlyEmployee) {
+		employeeRepository.saveAndFlush(convertHourlyEmployee(hourlyEmployee));
+	}
+
 }
